cfddd35659311a4ee38ba053430b50e76f9ee327
John Hennerich <john.hennerich@gmail.com>
2022-07-08 08:55:01 -0600

    remove api_key

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,8 +1,7 @@
-ActionMailer::Base.smtp_settings = {
-  address:        "smtp.sendgrid.net",
-  port:            587,
-  authentication: :plain,
-  user_name:      'apikey',
+#ActionMailer::Base.smtp_settings = {
+#  address:        "smtp.sendgrid.net",
+#  port:            587,
+#  authentication: :plain,
+#  user_name:      'apikey',
 #  password:       ENV['SENDGRID_API_KEY']
-  password: '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'
-}
+#}
